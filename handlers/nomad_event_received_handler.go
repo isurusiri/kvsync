d3caf4ed1b2ff7fb5e0ea1fadbd5f727072507c7
Isuru Siriwardana <isuruanatomy@gmail.com>
2021-10-05 10:45:30 +0530

    nomad job received handler

--- handlers/nomad_event_received_handler.go
+++ handlers/nomad_event_received_handler.go
@@ -1,1 +1,16 @@
 package handlers
+
+import "github.com/isurusiri/funnel/events"
+
+func init() {
+
+}
+
+type nomadJobReceivedHandler struct {
+	eventID string
+	jobID   string
+}
+
+func (n nomadJobReceivedHandler) Handle(payload events.NomadEventReceivedPayload) {
+
+}
