3eaa3ca847871aaf67e6225eece8d40b4bb4c060
Isuru Siriwardana <isuruanatomy@gmail.com>
2022-03-01 11:41:33 +0530

    started with unit testing

--- main_test.go
+++ main_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestCreateKeyWithJobID(t *testing.T) {
+	expect := "foo-service-version"
+	actual := createKeyWithJobID("foo-service", "version")
+
+	if expect != actual {
+		t.Errorf("Expected %q, actual %q", expect, actual)
+	}
+}
